11b6d0e39d7380d9995c72cb6590210d71c6bc64
Kre847 <105073466+Kre847@users.noreply.github.com>
2026-03-05 17:23:12 +0000

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-
-const CACHE = 'ww-v9-3';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./magician.png'];
-self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', (e) => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
